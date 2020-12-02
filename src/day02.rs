b7b3d792ecb124174384e579ab4daf09b8d6b047
Johan Guttormsson <johan@jgsoft.se>
2020-12-02 16:13:58 +0100

    day2 part2

--- src/day02.rs
+++ src/day02.rs
@@ -1,9 +1,16 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-fn is_valid_password(min: usize, max: usize, character: char, pwd: &str) -> bool {
+fn is_valid_password1(min: usize, max: usize, character: char, pwd: &str) -> bool {
     let n = pwd.matches(character).count();
     return n >= min && n <= max;
+}
+
+fn is_valid_password2(pos1: usize, pos2: usize, character: char, pwd: &str) -> bool {
+    let char1: char = pwd.chars().nth(pos1 - 1).unwrap();
+    let char2: char = pwd.chars().nth(pos2 - 1).unwrap();
+
+    return (char1 == character) ^ (char2 == character);
 }
 
 pub fn part1(input: String) {
@@ -11,8 +18,6 @@ pub fn part1(input: String) {
         static ref RE: Regex =
             Regex::new(r"(?P<min>\d+)-(?P<max>\d+) (?P<character>[A-Za-z]): (?P<pwd>.*)").unwrap();
     }
-    // let lines: Vec<(i32, i32, char, String)> =
-    //     input.lines().map(|s| s.parse::<i32>().unwrap()).collect();
 
     let mut valid_passwords = Vec::new();
     for line in input.lines() {
@@ -22,7 +27,7 @@ pub fn part1(input: String) {
         let character = caps["character"].parse::<char>().unwrap();
         let pwd = caps["pwd"].parse::<String>().unwrap();
 
-        if is_valid_password(min, max, character, &pwd) {
+        if is_valid_password1(min, max, character, &pwd) {
             valid_passwords.push(pwd);
         }
     }
@@ -30,7 +35,28 @@ pub fn part1(input: String) {
     println!("Number of valid passwords: {}", valid_passwords.len());
 }
 
-pub fn part2(input: String) {}
+pub fn part2(input: String) {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"(?P<pos1>\d+)-(?P<pos2>\d+) (?P<character>[A-Za-z]): (?P<pwd>.*)")
+                .unwrap();
+    }
+
+    let mut valid_passwords = Vec::new();
+    for line in input.lines() {
+        let caps = RE.captures(line).unwrap();
+        let min = caps["pos1"].parse::<usize>().unwrap();
+        let max = caps["pos2"].parse::<usize>().unwrap();
+        let character = caps["character"].parse::<char>().unwrap();
+        let pwd = caps["pwd"].parse::<String>().unwrap();
+
+        if is_valid_password2(min, max, character, &pwd) {
+            valid_passwords.push(pwd);
+        }
+    }
+
+    println!("Number of valid passwords: {}", valid_passwords.len());
+}
 
 #[cfg(test)]
 
@@ -38,15 +64,62 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_pwd1() {
+    fn test_part1_pwd1() {
         let min = 1;
         let max = 3;
         let character: char = 'a';
         let pwd = "abcde";
 
-        assert_eq!(is_valid_password(min, max, character, pwd), true);
+        assert_eq!(is_valid_password1(min, max, character, pwd), true);
     }
 
     #[test]
-    fn test_part2() {}
+    fn test_part1_pwd2() {
+        let min = 1;
+        let max = 3;
+        let character: char = 'b';
+        let pwd = "cdefg";
+
+        assert_eq!(is_valid_password1(min, max, character, pwd), false);
+    }
+
+    #[test]
+    fn test_part1_pwd3() {
+        let min = 2;
+        let max = 9;
+        let character: char = 'c';
+        let pwd = "ccccccccc";
+
+        assert_eq!(is_valid_password1(min, max, character, pwd), true);
+    }
+
+    #[test]
+    fn test_part2_pwd1() {
+        let min = 1;
+        let max = 3;
+        let character: char = 'a';
+        let pwd = "abcde";
+
+        assert_eq!(is_valid_password2(min, max, character, pwd), true);
+    }
+
+    #[test]
+    fn test_part2_pwd2() {
+        let min = 1;
+        let max = 3;
+        let character: char = 'b';
+        let pwd = "cdefg";
+
+        assert_eq!(is_valid_password2(min, max, character, pwd), false);
+    }
+
+    #[test]
+    fn test_part2_pwd3() {
+        let min = 2;
+        let max = 9;
+        let character: char = 'c';
+        let pwd = "ccccccccc";
+
+        assert_eq!(is_valid_password2(min, max, character, pwd), false);
+    }
 }
